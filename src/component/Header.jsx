dba9a841a15a6c106e856d982646861a36ce6d34
ritacc12 <rita20920@gmail.com>
2024-04-22 00:21:34 +0800

    create Header component

---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="isolate Header text-left">
+      <h2 className="text-4xl pb-1">To Do List</h2>
+      <p className="text-sm pb-3">Add things to do</p>
+    </div>
+  );
+};
+
+export default Header;
