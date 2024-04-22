0d7903c0195fd68f630a1a654cabff9e44891600
ritacc12 <rita20920@gmail.com>
2024-04-22 14:09:12 +0800

    set the status for TodoList, newTodoItem and completed status & feat AddTask function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./component/Header.jsx";
+import InputForm from "./component/InputForm.jsx";
+import ListPanel from "./component/ListPanel.jsx";
+import ProgressBar from "./component/ProgressBar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toDos, setToDos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  function handleAddTask() {
+    setToDos([...toDos, newArray]);
+    setNewArray("");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Header />
+      <ProgressBar toTos={toDos} />
+      <ListPanel />
+      <InputForm onAddTodo={handleAddTask} />
+    </div>
+  );
 }
 
-export default App
+export default App;
