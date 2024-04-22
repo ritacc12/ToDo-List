8b6e67b55443c9bffd576df958bc5d969b2e9cfc
ritacc12 <rita20920@gmail.com>
2024-04-22 20:02:50 +0800

    adjust the title of Todo List

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="isolate Header text-left">
-      <h2 className="text-4xl pb-1">To Do List</h2>
+      <h2 className="text-4xl pb-1">Todo List</h2>
       <p className="text-sm pb-3">Add things to do</p>
     </div>
   );
