e3f9f7bc8ea7baddc35c4a26280e26dee718517f
ritacc12 <rita20920@gmail.com>
2024-04-23 15:23:53 +0800

    import all component and add the addTodo , toggleTodo, deteleTodo function

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import Header from "./component/Header.jsx";
 import InputForm from "./component/InputForm.jsx";
 import ListPanel from "./component/ListPanel.jsx";
 import ProgressBar from "./component/ProgressBar.jsx";
+import ToggleButton from "./component/ToggleButton.jsx";
 
 function App() {
   const [toDos, setToDos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
   const [showCompleted, setShowCompleted] = useState(false);
 
-  function handleAddTask() {
-    setToDos([...toDos, newArray]);
-    setNewArray("");
+  function addTodo(text) {
+    const newTodo = {
+      id: uuidv4(), // 使用 uuidv4() 生成唯一的 ID
+      text,
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+    setToDos([...toDos, newTodo]);
   }
+
+  function toggleTodo(id) {
+    setToDos(
+      toDos.map((todo) =>
+        todo?.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  }
+
+  function deleteTodo(id) {
+    setToDos(toDos.filter((todo) => todo.id !== id));
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const todoList = document.querySelector(".todo-list");
+      if (todoList) {
+        todoList.scrollTop = todoList.scrollHeight;
+      }
+    };
+
+    if (toDos.length > 0) {
+      handleScroll();
+    }
+  }, [toDos]);
 
   return (
     <div className="app">
       <Header />
-      <ProgressBar toTos={toDos} />
-      <ListPanel />
-      <InputForm onAddTodo={handleAddTask} />
+      <ProgressBar toDos={toDos} />
+      <ListPanel
+        toDos={toDos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+        showCompleted={showCompleted}
+      />
+      <ToggleButton
+        showCompleted={showCompleted}
+        setShowCompleted={setShowCompleted}
+      />
+      <InputForm newTodo={newTodo} setNewTodo={setNewTodo} addTodo={addTodo} />
     </div>
   );
 }
